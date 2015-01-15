98b4c51af6391b7745ca4c13ffcf5cd384ea15e6
Matteo Collina <hello@matteocollina.com>
2015-01-15 12:41:41 +0000

    port, username, password to collector setup

--- srvs/info-prod.js
+++ srvs/info-prod.js
@@ -7,7 +7,7 @@ var influxIP = process.env.INFLUX_IP || 'localhost';
 require('seneca')()
   .use('redis-transport')
   .use('beanstalk-transport')
-  .use('collector', { host: influxIP, database: 'stats', seriesName: 'actions' })
+  .use('collector', { host: influxIP, database: 'stats', seriesName: 'actions', username: 'root', password: 'root', port: '8086' })
   .use('../info.js')
   .client({host: redisIP, type:'redis',pin:'role:info,req:part'})
   .listen({host: redisIP, type:'redis',pin:'role:info,res:part'})
